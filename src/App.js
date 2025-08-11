e9087c66cff45edb2c3b85a57d07cb57bb2f82c3
HudaifM <hudaifath.m@mountbox.in>
2025-08-11 13:39:53 +0530

    image to local

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [capturedImage, setCapturedImage] = useState('');
   
   // Replace this with a stored image URL from your DB or server
-  const storedImageUrl = 'https://res.cloudinary.com/dpwdruptp/image/upload/v123456789/group_photos/filename.jpg';
+  const storedImageUrl = 'http://localhost:5000/uploads/user1.jpg';
   return (
     <>
     <Router>
